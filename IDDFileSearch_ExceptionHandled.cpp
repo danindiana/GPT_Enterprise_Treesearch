caf473439edec0ad838f96c8242d8ee0f5fd843f
danindiana <danindiana@users.noreply.github.com>
2023-06-02 20:55:59 -0500

    Create IDDFileSearch_ExceptionHandled.cpp
    refactor that code to handle possible exceptions that may occur when accessing files or directories (for example, due to lack of permission) to make it more robust.
    
    This code catches std::filesystem::filesystem_error exceptions which can be thrown by the std::filesystem::directory_iterator constructor if it fails to open the directory (e.g., due to insufficient permissions). It prints an error message to the console and continues with the next directory/file.
    
    You will need a C++17 (or later) compatible compiler to compile this script. The compilation command for GCC would be g++ -std=c++17 IDDFileSearch_ExceptionHandled.cpp -o IDDFileSearch_ExceptionHandled.

---
+++ IDDFileSearch_ExceptionHandled.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <filesystem>
+#include <stack>
+#include <vector>
+
+// Recursive function to perform IDDFS
+bool DLS(const std::filesystem::path& currentPath, const std::string& target, int depth) {
+    try {
+        if (depth >= 0) {
+            for (const auto& dirEntry : std::filesystem::directory_iterator(currentPath)) {
+                if (depth == 0 && dirEntry.path().filename() == target) {
+                    std::cout << "Found file: " << dirEntry.path() << std::endl;
+                    return true;
+                }
+                else if (dirEntry.is_directory()) {
+                    if (DLS(dirEntry.path(), target, depth - 1)) {
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+    catch (const std::filesystem::filesystem_error& e) {
+        std::cout << "Error reading directory '" << currentPath << "': " << e.what() << std::endl;
+    }
+    return false;
+}
+
+// Function to perform IDDFS up to a maximum depth
+void IDDFS(const std::filesystem::path& rootPath, const std::string& target, int maxDepth) {
+    for (int depth = 0; depth <= maxDepth; depth++) {
+        if (DLS(rootPath, target, depth)) {
+            return;
+        }
+    }
+    std::cout << "File not found within depth " << maxDepth << std::endl;
+}
+
+int main() {
+    std::string targetFile = "target.txt"; // Your target file
+    std::filesystem::path rootPath = "/"; // Your root path
+    int maxDepth = 5; // Your max depth
+
+    IDDFS(rootPath, targetFile, maxDepth);
+
+    return 0;
+}
